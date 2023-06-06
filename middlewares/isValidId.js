6fea229eb6700d26709c8477963814ab087b1331
Aleksey-7 <84351678+Aleksey-7@users.noreply.github.com>
2023-06-06 19:59:12 +0300

    add avatars to users
    add avatars to users

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -5,7 +5,7 @@ const { HttpError } = require('../utils');
 const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    next(new HttpError(400, `${contactId} is not valid`));
+    return next(new HttpError(400, `${contactId} is not valid`));
   }
   next();
 };
--- services/authServices.js
+++ services/authServices.js
@@ -1,20 +1,28 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const path = require('path');
+const fs = require('fs/promises');
+const Jimp = require('jimp');
+require('dotenv').config();
 
 const User = require('../models/user');
 const { HttpError } = require('../utils');
+
+const avatarDir = path.join(__dirname, '../', 'public', 'avatars');
 const { SECRET_KEY } = process.env;
 
 const registerService = async body => {
   const { email, password } = body;
-  const currentUser = await User.findOne({ email });
-  if (currentUser) {
+  const user = await User.findOne({ email });
+  if (user) {
     throw new HttpError(409, 'Email already exists');
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = await gravatar.url(email);
 
-  const newUser = await User.create({ ...body, password: hashPassword });
+  const newUser = await User.create({ ...body, password: hashPassword, avatarURL });
   return newUser;
 };
 
@@ -51,9 +59,6 @@ const logoutService = async user => {
 };
 
 const updateSubscriptionService = async (contactId, body) => {
-  if (Object.keys(body).length === 0) {
-    throw new HttpError(400, 'missing field subscription');
-  }
   const user = await User.findByIdAndUpdate(contactId, body);
   if (!user) {
     throw new HttpError(404, 'Not found');
@@ -61,9 +66,31 @@ const updateSubscriptionService = async (contactId, body) => {
   return user;
 };
 
+const uploadAvatarService = async body => {
+  const { _id: id } = body.user;
+  const { path: tempUpload, originalname } = body.file;
+  const filename = `${id}_${originalname}`;
+  const resultUpload = path.join(avatarDir, filename);
+
+  await fs.rename(tempUpload, resultUpload);
+
+  (async function resize() {
+    const image = await Jimp.read(resultUpload);
+    image.resize(250, 250);
+    await image.writeAsync(resultUpload);
+  })();
+
+  const avatarURL = path.join('avatars', filename);
+
+  await User.findByIdAndUpdate(id, { avatarURL });
+
+  return avatarURL;
+};
+
 module.exports = {
   registerService,
   loginService,
   logoutService,
   updateSubscriptionService,
+  uploadAvatarService,
 };
