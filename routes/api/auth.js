1cc4ec785ed993a9979e3dc6b24b93a4deba5b2f
Aleksey-7 <84351678+Aleksey-7@users.noreply.github.com>
2023-06-11 21:20:30 +0300

    hw06-email and add Dockerfile
    hw06-email and add Dockerfile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const { validateBody, authenticate, upload } = require('../../middlewares');
 const {
   register,
+  verifyEmail,
+  resendVerifyEmail,
   login,
   getCurrent,
   logout,
@@ -14,9 +16,14 @@ const {
   createUserValidationSchema,
   loginValidationSchema,
   updateSubscriptionSchema,
+  verifyEmailSchema,
 } = require('../../schemas/authSchema');
 
 router.post('/register', validateBody(createUserValidationSchema), register);
+
+router.get('/verify/:verificationToken', verifyEmail);
+
+router.post('/verify', validateBody(verifyEmailSchema), resendVerifyEmail);
 
 router.post('/login', validateBody(loginValidationSchema), login);
 
--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const HttpError = require('./httpError');
 const handelMongooseError = require('./handelMongooseError');
 const controllerWrapper = require('./controllerWrapper');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
   HttpError,
   handelMongooseError,
   controllerWrapper,
+  sendEmail,
 };
